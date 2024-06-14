7050d4e1cd094544ad56dfd13e0b9e140b110bc6
Hamid Farznahe <hamid.farzaneh@tu-dresden.de>
2024-06-14 14:24:26 +0200

    .

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
-git clone --depth 1 https://github.com/oowekyala/llvm-project
-mkdir llvm-project/build 
-cd llvm-project/build
+git clone https://github.com/llvm/llvm-project
+cd llvm-project
+git checkout -b tags/llvmorg-18.1.6
+mkdir build   
+cd build
 
 cmake -G "Ninja" ../llvm \
   -DLLVM_ENABLE_PROJECTS="mlir;" \
@@ -13,3 +15,20 @@ cmake -G "Ninja" ../llvm \
   -DLLVM_OPTIMIZED_TABLEGEN=ON
 
 ninja
+# cd ../..
+# cd cinnamon 
+# llvm_prefix=../llvm-project/build
+
+# cmake -S . -B "build" \
+#     -G Ninja \
+#     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+#     -DLLVM_DIR="$llvm_prefix"/lib/cmake/llvm \
+#     -DMLIR_DIR="$llvm_prefix"/lib/cmake/mlir \
+#     -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+#     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+#     -DCMAKE_C_COMPILER=clang \
+#     -DCMAKE_LINKER_TYPE=DEFAULT \
+#     -DCMAKE_CXX_COMPILER=clang++ \
+#     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON 
+
+# cd build && ninja
