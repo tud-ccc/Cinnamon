8d1f9bbef18b5ab8fa36ddcceccb93f027274dc7
Clément Fournier <clement.fournier@tu-dresden.de>
2024-06-15 17:48:00 +0200

    Conditionally disable GPU lib support

--- cinnamon/include/cinm-mlir/Conversion/CnmToGPU/CnmToGPU.h
+++ cinnamon/include/cinm-mlir/Conversion/CnmToGPU/CnmToGPU.h
@@ -3,8 +3,11 @@
 #include <mlir/Pass/Pass.h>
 #include <mlir/Conversion/Passes.h>
 
+#ifdef CINM_GPU_SUPPORT
 namespace mlir::cnm {
     void populateCnmToGPUFinalTypeConversions(LLVMTypeConverter &typeConverter);
     void populateCnmToGPUConversionPatterns(LLVMTypeConverter &typeConverter, RewritePatternSet &patterns);
     std::unique_ptr<Pass> createConvertCnmToGPUPass();
 } // namespace mlir::cnm
+
+#endif
