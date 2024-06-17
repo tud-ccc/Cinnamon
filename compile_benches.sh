eed29110c8f9566d02bfe9d59834196a60b4187f
Clément Fournier <clement.fournier@tu-dresden.de>
2024-06-17 16:57:58 +0200

    Add app file for mlp

--- compile_benches.sh
+++ compile_benches.sh
@@ -1,15 +1,8 @@
-./cinnamon/build/bin/cinm-opt --cinm-tiling --cse ./cinnamon/testbench/va.mlir > ./cinnamon/testbench/generated/va.tiled.mlir
-./cinnamon/build/bin/cinm-opt --cinm-tiling --cse ./cinnamon/testbench/va.mlir 
-./cinnamon/build/bin/cinm-opt --convert-cinm-to-cnm --func-bufferize --lower-affine --cse ./cinnamon/testbench/generated/va.tiled.mlir > ./cinnamon/testbench/generated/va.cnm.mlir
-./cinnamon/build/bin/cinm-opt --convert-cinm-to-cnm --func-bufferize --lower-affine --cse ./cinnamon/testbench/generated/va.tiled.mlir 
-./cinnamon/build/bin/cinm-opt --convert-cnm-to-upmem -convert-linalg-to-affine-loops --lower-affine --upmem-outline-kernel --cse ./cinnamon/testbench/generated/va.cnm.mlir 
-./cinnamon/build/bin/cinm-opt --convert-cnm-to-upmem -convert-linalg-to-affine-loops --lower-affine --upmem-outline-kernel --cse ./cinnamon/testbench/generated/va.cnm.mlir > ./cinnamon/testbench/generated/va.upmem.mlir
-./cinnamon/build/bin/cinm-translate --mlir-to-upmem-cpp ./cinnamon/testbench/generated/va.upmem.mlir > ./cinnamon/testbench/generated/va_dpu.c
-./cinnamon/build/bin/cinm-translate --mlir-to-upmem-cpp ./cinnamon/testbench/generated/va.upmem.mlir 
 
-# ./cinnamon/build/bin/cinm-opt ./cinnamon/testbench/generated/va.upmem.mlir \
-#     --canonicalize --convert-scf-to-cf --convert-cf-to-llvm --convert-arith-to-llvm \
-# 	--convert-upmem-to-llvm \
-#     --fold-memref-alias-ops --expand-strided-metadata --memref-expand --finalize-memref-to-llvm \
-# 	--convert-func-to-llvm=use-bare-ptr-memref-call-conv=true --cse --reconcile-unrealized-casts --cse > ./cinnamon/testbench/generated/va.host.ll
+cd cinnamon/testbench
+BENCH_NAME=va make
+BENCH_NAME=mlp make
+BENCH_NAME=1mm make
+BENCH_NAME=2mm make
+BENCH_NAME=3mm make
 
