e6bf5a38b559202f100eaefc4e2c4351f3e69357
Clément Fournier <clement.fournier@tu-dresden.de>
2024-06-22 15:24:20 +0200

    Use just in compile_benches

--- compile_benches.sh
+++ compile_benches.sh
@@ -2,17 +2,9 @@
 # source /opt/upmem/upmem-2023.2.0-Linux-x86_64/upmem_env.sh
 
 cd cinnamon/testbench
-BENCH_NAME=va make clean
-BENCH_NAME=mlp make clean
-BENCH_NAME=1mm make clean
-BENCH_NAME=2mm make clean
-BENCH_NAME=3mm make clean
-BENCH_NAME=mv make clean
-
-BENCH_NAME=va make
-BENCH_NAME=mlp make
-BENCH_NAME=1mm make
-BENCH_NAME=2mm make
-BENCH_NAME=3mm make
-BENCH_NAME=mv make
-
+just genBench va
+just genBench mlp
+just genBench 1mm
+just genBench 2mm
+just genBench 3mm
+just genBench mv
