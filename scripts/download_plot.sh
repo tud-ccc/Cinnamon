21b7830d6fc9626db5cafa74cf3fb0cf94aea565
Hamid Farzaneh <hamidfarzaneh1996@gmail.com>
2024-06-22 10:23:57 +0200

    update download_plot script

--- scripts/download_plot.sh
+++ scripts/download_plot.sh
@@ -2,11 +2,13 @@
 
 # Get the directory where the script is located
 
-MACHINE_DIR="/home/reviwer/Cinnamon/plot/" # Modify if you have cloned your own version
+MACHINE_DIR="/home/reviewer/Cinnamon/plot/" # Modify if you have cloned your own version
 
 TARGET_DIR="." # modify to your liking
 SSH_HOST="reviewer@ios.inf.uni-osnabrueck.de"
 SSH_PORT="2293"
 
+SOURCE_DIR="reviewer@ios.inf.uni-osnabrueck.de:$MACHINE_DIR"
+
 # # Use scp to copy files
-scp -P $SSH_PORT -r "$TARGET_DIR" "$MACHINE_DIR" 
+scp -P $SSH_PORT -r "$SOURCE_DIR" "$TARGET_DIR" 
