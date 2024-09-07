8c5ee91234d8629448c17cfd4a68f1ee15e0391a
Felix Reissmann <felix.rm.153@gmail.com>
2024-09-08 00:28:38 +0200

    Add torch-mlir to build and configure scripts.

--- .github/workflows/build-ci.sh
+++ .github/workflows/build-ci.sh
@@ -3,19 +3,49 @@
 project_root="$( cd -- "$(dirname "$0")/../.." >/dev/null 2>&1 ; pwd -P )"
 echo "Project root: $project_root"
 
+py_venv_path="$project_root/.venv"
 cinnamon_path="$project_root/cinnamon"
 llvm_path="$project_root/llvm"
+torch_mlir_path="$project_root/torch-mlir"
 upmem_path="$project_root/upmem"
 
+setup_python_venv=true
 checkout_and_build_llvm=true
+checkout_and_build_torch_mlir=true
 checkout_upmem=true
+
+if echo "$@" | grep -q "no-python-venv"; then
+  setup_python_venv=false
+fi
 
 if echo "$@" | grep -q "no-llvm"; then
   checkout_and_build_llvm=false
 fi
 
+if echo "$@" | grep -q "no-torch-mlir"; then
+  checkout_and_build_torch_mlir=false
+fi
+
 if echo "$@" | grep -q "no-upmem"; then
   checkout_upmem=false
+fi
+
+if [[ $setup_python_venv ]]; then
+  deactivate 2> /dev/null
+
+  if [ ! -d "$py_venv_path" ]; then
+    python3 -m venv "$py_venv_path"
+    source "$py_venv_path/bin/activate"
+    pip install --upgrade pip
+    # https://pytorch.org/get-started/locally/
+    pip install torch --index-url https://download.pytorch.org/whl/cpu
+    pip install numpy pybind11
+  else
+    source "$py_venv_path/bin/activate"
+  fi
+else
+  echo "Skipping Python venv setup"
+  echo "Make sure to have a correct Python environment set up"
 fi
 
 if [[ checkout_and_build_llvm ]]; then
@@ -29,7 +59,7 @@ if [[ checkout_and_build_llvm ]]; then
       -DLLVM_ENABLE_PROJECTS="mlir;llvm;clang" \
       -DLLVM_TARGETS_TO_BUILD="host" \
       -DLLVM_ENABLE_ASSERTIONS=ON \
-      -DMLIR_ENABLE_BINDINGS_PYTHON=OFF \
+      -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
       -DLLVM_BUILD_TOOLS=OFF \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_SHARED_LIBS=ON \
@@ -46,6 +76,35 @@ if [[ checkout_and_build_llvm ]]; then
 else
   echo "Skipping LLVM checkout and build"
   echo "The following steps will need LLVM_DIR and MLIR_DIR to be set in their respective <STEP>_CMAKE_OPTIONS"
+fi
+
+if [[ checkout_and_build_torch_mlir ]]; then
+  if [ ! -d "$torch_mlir_path" ]; then
+    git clone https://github.com/llvm/torch-mlir "$torch_mlir_path"
+
+    cd "$torch_mlir_path"
+
+    dependency_paths=""
+
+    if [[ checkout_and_build_llvm ]]; then
+      dependency_paths="$dependency_paths -DLLVM_DIR=$llvm_path/build/lib/cmake/llvm"
+      dependency_paths="$dependency_paths -DMLIR_DIR=$llvm_path/build/lib/cmake/mlir"
+    fi
+
+    git checkout snapshot-20240127.1096
+    cmake -S . -B build \
+      $dependency_paths \
+      -DTORCH_MLIR_OUT_OF_TREE_BUILD=ON \
+      -DTORCH_MLIR_ENABLE_STABLEHLO=OFF \
+      $TORCH_MLIR_CMAKE_OPTIONS
+  fi
+
+  cd "$torch_mlir_path"
+  cmake --build build
+  cmake --install build --prefix install
+else
+  echo "Skipping Torch-MLIR checkout and build"
+  echo "The following steps will need TORCH_MLIR_DIR to be set in their respective <STEP>_CMAKE_OPTIONS"
 fi
 
 if [[ checkout_upmem ]]; then
@@ -70,6 +129,10 @@ if [ ! -d "build" ]; then
   if [[ checkout_and_build_llvm ]]; then
     dependency_paths="$dependency_paths -DLLVM_DIR=$llvm_path/build/lib/cmake/llvm"
     dependency_paths="$dependency_paths -DMLIR_DIR=$llvm_path/build/lib/cmake/mlir"
+  fi
+
+  if [[ checkout_and_build_torch_mlir ]]; then
+    dependency_paths="$dependency_paths -DTORCH_MLIR_DIR=$torch_mlir_path/install"  
   fi
 
   if [[ checkout_upmem ]]; then
