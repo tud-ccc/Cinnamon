20df3c5fd5af72eabb6c2eae0fb7bbab315ef494
Felix Reissmann <felix.rm.153@gmail.com>
2024-10-13 23:40:33 +0200

    Add improved scheduling infrastructure.

---
+++ cinnamon/include/cinm-mlir/Utils/Scheduling/Schedulers/Alap.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <llvm/ADT/ArrayRef.h>
+#include <llvm/ADT/STLExtras.h>
+
+#include <cstddef>
+#include <cstdio>
+#include <iostream>
+#include <limits>
+#include <vector>
+
+#include "cinm-mlir/Utils/Scheduling/Scheduling.h"
+
+namespace mlir::cim::scheduling {
+
+  template<typename ComputeResource>
+  class AlapScheduler {
+   public:
+    AlapScheduler(llvm::ArrayRef<ComputeResource> resources)
+        : computeResources(std::move(resources)) {}
+
+    OperationScheduling<ComputeResource> schedule(DependencyGraphView graph) {
+      OperationScheduling<ComputeResource> schedule;
+
+      bool doneScheduling = false;
+
+      size_t time = std::numeric_limits<size_t>::max();
+      for (; !doneScheduling; --time) {
+        doneScheduling = true;
+
+        size_t nextFreeResource = 0;
+        std::vector<DependencyGraphNode *> scheduledThisTime{};
+
+        for (auto &node : graph) {
+          if (node.scheduled)
+            continue;
+
+          if (!llvm::all_of(node.dependents, isScheduled))
+            continue;
+
+          if (nextFreeResource < computeResources.size()) {
+            schedule.push_back(ScheduledOperation<ComputeResource>{
+                .operation = &node.operation,
+                .resource = computeResources[nextFreeResource++],
+                .dispatchTime = time,
+                .barrierTime = time});
+
+            scheduledThisTime.push_back(&node);
+          }
+
+          doneScheduling = false;
+        }
+
+        for (auto *node : scheduledThisTime)
+          node->scheduled = true;
+      }
+
+      for (auto &scheduledOp : schedule) {
+        scheduledOp.dispatchTime = scheduledOp.dispatchTime - time;
+        scheduledOp.barrierTime = scheduledOp.barrierTime - time;
+      }
+
+      return schedule;
+    }
+
+   private:
+    static bool isScheduled(DependencyGraphNode const *n) { return n->scheduled; }
+
+    llvm::ArrayRef<ComputeResource> computeResources;
+  };
+}  // namespace mlir::cim::scheduling
---
+++ cinnamon/include/cinm-mlir/Utils/Scheduling/Scheduling.h
@@ -0,0 +1,144 @@
+#pragma once
+
+#include <llvm/ADT/ArrayRef.h>
+#include <llvm/ADT/STLExtras.h>
+#include <mlir/IR/Location.h>
+#include <mlir/IR/Operation.h>
+#include <mlir/IR/PatternMatch.h>
+#include <mlir/IR/ValueRange.h>
+
+#include <algorithm>
+#include <compare>
+#include <cstddef>
+#include <cstdio>
+#include <functional>
+#include <limits>
+#include <list>
+#include <vector>
+
+#include "cinm-mlir/Dialect/Cim/IR/CimDialect.h"
+#include "cinm-mlir/Dialect/Cim/Transforms/Passes.h"
+
+namespace mlir::cim::scheduling {
+
+  struct DependencyGraphNode {
+    Operation &operation;
+    std::vector<DependencyGraphNode *> dependencies{};
+    std::vector<DependencyGraphNode *> dependents{};
+    bool scheduled{false};
+  };
+
+  using DependencyGraph = std::list<DependencyGraphNode>;
+  using DependencyGraphView = DependencyGraph &;
+
+  template<typename ComputeResource>
+  struct ScheduledOperation {
+    Operation *operation;
+    ComputeResource resource;
+    size_t dispatchTime, barrierTime;
+
+    std::strong_ordering operator<=>(ScheduledOperation const &other) {
+      return dispatchTime <=> other.dispatchTime;
+    }
+
+    friend void swap(ScheduledOperation &lhs, ScheduledOperation &rhs) {
+      using std::swap;
+      swap(lhs.operation, rhs.operation);
+      swap(lhs.resource, rhs.resource);
+      swap(lhs.dispatchTime, rhs.dispatchTime);
+      swap(lhs.barrierTime, rhs.barrierTime);
+    }
+  };
+
+  template<typename ComputeResource>
+  using OperationScheduling = std::list<ScheduledOperation<ComputeResource>>;
+
+  template<typename ComputeResource>
+  struct SchedulingHooks {
+    using RescheduleOperationFilter = std::function<bool(Operation &op)>;
+    using SchedulingStrategy = std::function<OperationScheduling<ComputeResource>(
+        DependencyGraphView graph)>;
+    using OperationScheduler = std::function<void(
+        Operation &operation, ComputeResource schedulingResource)>;
+    using BarrierInserter =
+        std::function<Operation *(PatternRewriter &rewriter, Operation &op)>;
+
+    RescheduleOperationFilter rescheduleOperationFilter;
+    SchedulingStrategy schedulingStrategy;
+    OperationScheduler operationScheduler;
+    BarrierInserter barrierInserter;
+  };
+
+  template<typename ComputeResource>
+  void applyScheduling(PatternRewriter &rewriter, ValueRange rootValues,
+                       SchedulingHooks<ComputeResource> const &hooks) {
+    std::list<DependencyGraphNode *> openOperations{};
+    DependencyGraph dependencyGraph{};
+
+    for (auto const &value : rootValues) {
+      DependencyGraphNode node{.operation = *value.getDefiningOp()};
+      auto it = dependencyGraph.insert(dependencyGraph.end(), std::move(node));
+      openOperations.push_back(&*it);
+    }
+
+    while (!openOperations.empty()) {
+      auto &currentNode = *openOperations.front();
+      openOperations.pop_front();
+
+      for (auto value : currentNode.operation.getOperands()) {
+        auto *definingOp = value.getDefiningOp();
+
+        if (!definingOp || !hooks.rescheduleOperationFilter(*definingOp))
+          continue;
+
+        DependencyGraphNode node{.operation = *definingOp};
+        node.dependents.push_back(&currentNode);
+        auto it = dependencyGraph.insert(dependencyGraph.end(), std::move(node));
+
+        openOperations.push_back(&*it);
+        currentNode.dependencies.push_back(&*it);
+      }
+    }
+
+    auto schedule = hooks.schedulingStrategy(dependencyGraph);
+
+    size_t currentTime = 0;
+    Operation *lastOperation = nullptr;
+
+    static constexpr size_t noTime = std::numeric_limits<size_t>::max();
+
+    while (!schedule.empty()) {
+      // First pass: Only op dispatch
+      for (auto &scheduledOp : schedule) {
+        if (scheduledOp.dispatchTime != currentTime)
+          continue;
+
+        scheduledOp.dispatchTime = noTime;
+        Operation *op = scheduledOp.operation;
+        if (lastOperation)
+          op->moveAfter(lastOperation);
+        lastOperation = op;
+        hooks.operationScheduler(*op, scheduledOp.resource);
+      }
+
+      // Second pass: Generate barriers
+      for (auto it = schedule.begin(); it != schedule.end();) {
+        auto scheduledOp{it++};
+
+        if (scheduledOp->barrierTime != currentTime)
+          continue;
+
+        rewriter.setInsertionPointAfter(lastOperation);
+        auto *barrierOp = hooks.barrierInserter(rewriter, *scheduledOp->operation);
+
+        if (barrierOp)
+          lastOperation = barrierOp;
+
+        schedule.erase(scheduledOp);
+      }
+
+      currentTime++;
+    }
+  }
+
+}  // namespace mlir::cim::scheduling
