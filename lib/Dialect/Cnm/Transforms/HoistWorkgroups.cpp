f35d90d9f8f2c07a49b4ad4fe4e19379c98f751d
Clément Fournier <clement.fournier@tu-dresden.de>
2026-05-13 14:29:12 +0200

    Fix testbench script

--- lib/Dialect/Cnm/Transforms/HoistWorkgroups.cpp
+++ lib/Dialect/Cnm/Transforms/HoistWorkgroups.cpp
@@ -6,6 +6,7 @@
 #include <mlir/Dialect/Func/IR/FuncOps.h>
 #include <mlir/IR/Builders.h>
 #include <mlir/IR/IRMapping.h>
+#include <mlir/IR/OpDefinition.h>
 #include <mlir/Interfaces/ControlFlowInterfaces.h>
 
 namespace mlir::cnm {
@@ -32,23 +33,22 @@ struct CnmHoistWorkgroupsPass
     IRMapping mapper;
     for (auto alloc : allocs) {
       Operation *parent = alloc;
-      while (parent->getParentOp() != fun) {
+      while (parent->getParentOp() && parent->getParentOp() != fun && !parent->getParentOp()->hasTrait<OpTrait::IsIsolatedFromAbove>()) {
         parent = parent->getParentOp();
       }
       if (parent == alloc) {
         // nothing to hoist
         continue;
       }
+      alloc->remove();
       rewriter.setInsertionPoint(parent);
-      auto newAlloc = rewriter.clone(*alloc, mapper);
-      alloc.replaceAllUsesWith(newAlloc);
-      alloc->erase();
+      rewriter.insert(alloc);
 
-      for (auto user : newAlloc->getUsers()) {
+      for (auto user : alloc->getUsers()) {
         if (llvm::isa<cnm::FreeWorkgroupOp>(user)) {
+          user->remove();
           rewriter.setInsertionPointAfter(parent);
-          rewriter.clone(*user, mapper);
-          user->erase();
+          rewriter.insert(user);
           break;
         }
       }
