f66ebebf22eda868a12f9ecc8f1180a1b4dff5e3
Clément Fournier <clement.fournier@tu-dresden.de>
2026-05-14 14:56:40 +0200

    Add GemmLikeOpInterface

---
+++ include/cinm-mlir/Dialect/Cinm/IR/CinmGemmlikeOpInterface.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/Value.h"
+
+//===- Generated includes -------------------------------------------------===//
+
+#include "cinm-mlir/Dialect/Cinm/IR/CinmGemmlikeOpInterface.h.inc"
+
+//===----------------------------------------------------------------------===//
