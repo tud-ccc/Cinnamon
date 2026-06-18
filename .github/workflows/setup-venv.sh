f2052a0a6cec165a00f45a55c7c6afb8a367a83a
Clément Fournier <clement.fournier@tu-dresden.de>
2026-06-18 18:56:59 +0200

    Move conan recipes

--- .github/workflows/setup-venv.sh
+++ .github/workflows/setup-venv.sh
@@ -58,6 +58,7 @@ if [[ "$setup_python_venv" -eq 1 ]]; then
     # Note this needs to be done after LLVM has been cloned.
     verbose_cmd python -m pip install -r third-party/llvm/mlir/python/requirements.txt
     verbose_cmd python -m pip install -r experiments/requirements.txt
+    verbose_cmd conan export third-party/conan-recipes/mlpack --name mlpack --version 4.8.0 
   fi
 
   # Ensure CMake will use this venv's Python and find pybind11's CMake config
