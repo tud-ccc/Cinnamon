42d2111dd2885b55a8c4f224c4d5363eb37ca928
Clément Fournier <clement.fournier@tu-dresden.de>
2026-08-16 12:33:29 +0200

    Model launch overhead

--- lib/Dialect/UPMEM/Transforms/UpmemPythonSimulator.cpp
+++ lib/Dialect/UPMEM/Transforms/UpmemPythonSimulator.cpp
@@ -537,7 +537,8 @@ struct CppSimulator : UpmemSimulator {
       auto hierarchy =
           llvm::cast<DeviceHierarchyType>(waitFor.getDpuSet().getType());
       int numDpus = hierarchy.getNumDpus();
-      auto launchOverhead = 0;
+      int numRanks = numDpus / 64;
+      double launchOverhead = 0.0389 + 0.00758 * numRanks;
       // auto launchOverhead = 0.0254524 * numDpus / 64;
       // auto launchOverhead = 0.041958 * log2(numDpus);
       // auto launchOverhead =
